0cce885327d1b43510e5cbfec19b72096a544701
balamurugan678@yahoo.co.in <GBala678#>
2017-04-19 15:40:32 +0100

    Added .gitignore

--- src/main/scala/com/hsbc/dsl/models/Composer.scala
+++ src/main/scala/com/hsbc/dsl/models/Composer.scala
@@ -3,6 +3,4 @@ package com.hsbc.dsl.models
 /**
   * Created by Bala on 19/04/2017.
   */
-case class Composer(name: String, birthplace: String, compositions: Seq[String])
-
 case class Person(firstname:String, lastname:String)
